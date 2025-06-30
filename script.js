079bc2d503a67646dfba1871dab1e2101e944db3
Arkodev Mukherjee <78291548+ArkodevMukherjee@users.noreply.github.com>
2025-06-30 23:55:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function authenticate(endpoint) {
 
   console.log(password)
 
-  const res = await fetch(`http://localhost:5000/${endpoint}`, {
+  const res = await fetch(`https://luna-backend-f7f6ggevc6dxe8dd.canadacentral-01.azurewebsites.net/${endpoint}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
